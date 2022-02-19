8481c2aed7a3d9638769123ed06f1341acb65012
Vashishth Patel <vashishthchaudhary48@gmail.com>
2022-02-19 12:24:02 +0530

    added issue close greeter bot

---
+++ bot/issue_close_greet.py
@@ -0,0 +1,35 @@
+import asyncio
+import os
+import sys
+import traceback
+import aiohttp
+from aiohttp import web
+from gidgethub import aiohttp as gh_aiohttp
+from gidgethub import routing
+from gidgethub import sansio
+from gidgethub import apps
+
+
+router = routing.Router()
+
+
+@router.register("issues", action="closed")
+async def issue_opened_event(event, gh, *args, **kwargs):
+
+    installation_id = event.data["installation"]["id"]
+
+    installation_access_token = await apps.get_installation_access_token(
+        gh,
+        installation_id=installation_id,
+        app_id=os.environ.get("GH_APP_ID"),
+        private_key=os.environ.get("GH_PRIVATE_KEY")
+    )
+    url = event.data['issue']['comments_url']
+    author = event.data['issue']['user']['login']
+    message = f"<br><table><tbody><tr><td>Thanks for closing this issue @{author}! We hope you loved to work with our repository 😋.</td></tr></tbody></table>"
+    
+    await gh.post(url, data={
+        'body': message,
+        },
+        oauth_token=installation_access_token["token"]
+                 )
