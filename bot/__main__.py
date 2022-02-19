fbd3ba4c186b9fc0a63cda1626ffdfaa9fdae5e8
Vashishth Patel <vashishthchaudhary48@gmail.com>
2022-02-19 12:15:49 +0530

    added issue greeter bot and done listing

--- bot/__main__.py
+++ bot/__main__.py
@@ -12,6 +12,11 @@ from gidgethub import routing
 from gidgethub import sansio
 from gidgethub import apps
 
+from . import issue_greeter
+
+router = routing.Router(issue_greeter.router, pr_greeter.router, issue_labeler_review_needed.router, pr_review_needed_labeler.router, comment_reacter.router, issue_close_greet.router, pr_close_greet.router, issue_assign.router, workflow_status.router)
+
+
 cache = cachetools.LRUCache(maxsize=500)
 
 routes = web.RouteTableDef()
---
+++ bot/issue_greeter.py
@@ -0,0 +1,37 @@
+import asyncio
+import os
+import sys
+import traceback
+import aiohttp
+from aiohttp import web
+from gidgethub import aiohttp as gh_aiohttp
+from gidgethub import routing
+from gidgethub import sansio
+from gidgethub import apps
+
+router = routing.Router()
+
+@router.register("issues", action="opened")
+async def issue_opened_event(event, gh, *args, **kwargs):
+
+    installation_id = event.data["installation"]["id"]
+
+    installation_access_token = await apps.get_installation_access_token(
+        gh,
+        installation_id=installation_id,
+        app_id=os.environ.get("GH_APP_ID"),
+        private_key=os.environ.get("GH_PRIVATE_KEY")
+    )
+
+    url = event.data['issue']['comments_url']
+    
+    author = event.data['issue']['user']['login']
+    
+    avatar = event.data['issue']['user']['avatar_url']
+
+    message = f"<br><table><tbody><tr><td>Thanks for opening the issue @{author}! I will look into it ASAP!\n Till then show your love by staring my repos 😋<br>Please assign this issue to you by commenting `/assign` .</td><td> <img alt='Coding' width='100px' height='100px' src='{avatar}'></td></tr></tbody></table>"
+    await gh.post(url, data={
+        'body': message,
+        },
+        oauth_token=installation_access_token["token"]
+                 )
