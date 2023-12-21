1f88c932f0949d80cc1adb39c222691a5855843f
Vashishth Patel <vashishthchaudhary48@gmail.com>
2023-12-21 17:34:05 +0530

    added line

--- bot/pr_greeter.py
+++ bot/pr_greeter.py
@@ -27,7 +27,7 @@ async def opened_pr(event, gh, *arg, **kwargs):
     author = event.data['pull_request']['user']['login']
     ur = event.data['pull_request']['comments_url']
 
-    messag = f"<b> Hi @author 👋<br>Thanks for opening this pull request. :octocat: </b><br>I will look into it soon! Till then you can improve your code & you can show your love by staring [my repos](https://github.com/vasu-1?tab=repositories). 😋<br><i>By contributing, you are expected to uphold this [code of conduct](https://github.com/vasu-1/CalcHub/blob/main/CODE_OF_CONDUCT.md)."
+    messag = f"<b> Hi @author 👋<br>Thanks for opening this pull request. :octocat: </b><br>I will look into it soon! Till then you can improve your code & show your love by staring [my repos](https://github.com/vasu-1?tab=repositories). 😋<br><i>By contributing, you are expected to uphold this [code of conduct](https://github.com/vasu-1/CalcHub/blob/main/CODE_OF_CONDUCT.md)."
     await gh.post(ur, data={
         'body': messag,
         },
